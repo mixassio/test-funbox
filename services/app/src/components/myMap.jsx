2c567abc8db4250a9809ab2a61a5e0575f1386a1
mixassio <mixail.bondarev@gmail.com>
2019-04-03 17:52:55 +0300

    drag

--- services/app/src/components/myMap.jsx
+++ services/app/src/components/myMap.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/no-unused-state */
 import React from 'react';
-import { YMaps, Map } from 'react-yandex-maps';
+import {
+  YMaps, Map, Placemark, Polyline,
+} from 'react-yandex-maps';
 import connect from '../connect';
+import pointsSelector from '../selectors';
 
 const mapStateToProps = state => ({
   setCurrentCenter: state.setCurrentCenter,
   currentCenter: state.currentCenter,
+  points: pointsSelector(state),
 });
 
 @connect(mapStateToProps)
@@ -15,17 +19,34 @@ class MyMap extends React.Component {
     setCurrentCenter({ center: event.get('newCenter'), zoom: event.get('newZoom') });
   };
 
+  handleClick2 = event => console.log(event, event.get('target')['geometry']['_coordinates']);
+
+
   render() {
-    const { currentCenter } = this.props;
+    const { currentCenter, points } = this.props;
     return (
       <YMaps>
         <div>
-          <Map
-            state={currentCenter}
-            width={500}
-            height={500}
-            onBoundschange={this.handleClick}
-          />
+          <Map state={currentCenter} width={500} height={500} onBoundschange={this.handleClick}>
+            {points.map(({ center, id, name }) => (
+              <Placemark
+                key={id}
+                geometry={center}
+                properties={{ balloonContentBody: 'name' }}
+                options={{ draggable: true }}
+                onDragend={this.handleClick2}
+              />
+            ))}
+            <Polyline
+              geometry={points.map(({ center }) => center)}
+              options={{
+                balloonCloseButton: true,
+                strokeColor: '#000',
+                strokeWidth: 4,
+                strokeOpacity: 0.5,
+              }}
+            />
+          </Map>
         </div>
       </YMaps>
     );
